21018b305715ce069b2b88d10b7f958d35af465d
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-09-02 15:56:56 +0530

    Create Jokes Db, Entity and Dao

---
+++ app/src/main/java/com/example/jokesapp/data/local/JokesEntity.kt
@@ -0,0 +1,15 @@
+package com.example.jokesapp.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "jokes_entity")
+data class JokesEntity(
+    @PrimaryKey
+    val id: Int,
+    val type: String,
+    val setup: String?,
+    val punchLine: String?,
+    val jokeMessage: String?,
+    val isBookMarked: Boolean = false
+)
