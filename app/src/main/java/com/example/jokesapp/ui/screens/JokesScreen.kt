da6380161a91618f85fa04fd47a7beffe3a4b98c
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-09-06 14:43:38 +0530

    Create Swipe to Dismiss

--- app/src/main/java/com/example/jokesapp/ui/screens/JokesScreen.kt
+++ app/src/main/java/com/example/jokesapp/ui/screens/JokesScreen.kt
@@ -1,11 +1,27 @@
 package com.example.jokesapp.ui.screens
 
+import android.view.View
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxState
 import androidx.compose.material3.SwipeToDismissBoxValue
@@ -16,12 +32,21 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.jokesapp.R
 import com.example.jokesapp.data.local.JokesEntity
 import com.example.jokesapp.ui.viewmodel.JokesViewmodel
+import com.example.jokesapp.util.CustomRowWith2Values
 import com.example.jokesapp.util.ErrorMessage
 import com.example.jokesapp.util.LoadIndicator
+import com.example.jokesapp.util.addSoundEffect
 import com.example.jokesapp.util.toastMsg
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -92,24 +117,120 @@ fun JokesScreen(
                         SwipeToDismissBox(state = dismissState, backgroundContent = {
                             SwipeToDismissBackgroundContent(dismissState)
                         }) {
-                            JokeItem(unbookmarkedJoke = joke)
-                            TODO()
+                            JokeItem(unbookmarkedJoke = joke) { isBookmarked ->
+                                viewModel.updateBookmarkStatus(joke.id, isBookmarked)
+                                toastMsg(context = context, msg = "Joke Bookmarked")
+                            }
                         }
                     }
                 }
             }
         }
+
         else -> {}
     }
 }
 
 @Composable
-fun JokeItem(unbookmarkedJoke: JokesEntity) {
-    TODO()
+fun JokeItem(
+    unbookmarkedJoke: JokesEntity,
+    jokePressed: (JokesEntity) -> Unit = {},
+    updateBookmark: (Boolean) -> Unit
+) {
+    val view: View = LocalView.current
+    Card(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()
+        .clickable {
+            addSoundEffect(view)
+            jokePressed(unbookmarkedJoke)
+        }
+    ) {
+        Row(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .padding(8.dp)
+        ) {
+            if (unbookmarkedJoke.type == "single") {
+                unbookmarkedJoke.jokeMessage?.let { joke ->
+
+                    CustomRowWith2Values(
+                        modifier = Modifier.weight(1f),
+                        value1 = "Joke",
+                        value2 = joke
+                    )
+                }
+            } else {
+                Column(modifier = Modifier.weight(1f)) {
+                    unbookmarkedJoke.setup?.let { setup ->
+                        CustomRowWith2Values(value1 = "Setup", value2 = setup)
+                        unbookmarkedJoke.punchLine?.let { punchline ->
+                            CustomRowWith2Values(value1 = "Punchline", value2 = punchline)
+                        }
+                    }
+                }
+            }
+            IconToggleButton(checked = unbookmarkedJoke.isBookMarked,
+                onCheckedChange = { value ->
+                    addSoundEffect(view = view)
+                    updateBookmark(value)
+                }) {
+                Icon(
+                    painter =
+                    if (unbookmarkedJoke.isBookMarked)
+                        painterResource(id = R.drawable.baseline_bookmark_add_24)
+                    else painterResource(id = R.drawable.outline_bookmarks_24),
+                    contentDescription = "book mark"
+                )
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SwipeToDismissBackgroundContent(dismissState: SwipeToDismissBoxState) {
-    TODO()
+    val backgroundColor by animateColorAsState(
+        when (dismissState.targetValue) {
+            SwipeToDismissBoxValue.EndToStart -> Color.Red.copy(alpha = 0.8f)
+            SwipeToDismissBoxValue.StartToEnd -> Color.Green
+            else -> Color.White
+        }, label = ""
+    )
+    // icon size
+    val iconScale by animateFloatAsState(
+        targetValue = if (
+            dismissState.targetValue == SwipeToDismissBoxValue.EndToStart ||
+            dismissState.targetValue == SwipeToDismissBoxValue.StartToEnd
+        ) 1.3f else 0.5f,
+        label = ""
+    )
+    Box(
+        Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .clip(RoundedCornerShape(16.dp))
+            .background(color = backgroundColor)
+            .padding(16.dp),
+    ) {
+        if (dismissState.targetValue == SwipeToDismissBoxValue.StartToEnd) {
+            Icon(
+                modifier = Modifier
+                    .scale(iconScale)
+                    .align(Alignment.CenterStart),
+                painter = painterResource(id = R.drawable.outline_bookmark_add_24),
+                contentDescription = "Bookmark",
+                tint = Color.White
+            )
+        } else {
+            Icon(
+                modifier = Modifier
+                    .scale(iconScale)
+                    .align(Alignment.CenterEnd),
+                imageVector = Icons.Outlined.Delete,
+                contentDescription = "Delete",
+                tint = Color.White
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/jokesapp/util/Utils.kt
+++ app/src/main/java/com/example/jokesapp/util/Utils.kt
@@ -1,13 +1,19 @@
 package com.example.jokesapp.util
 
 import android.content.Context
+import android.view.SoundEffectConstants
+import android.view.View
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -27,4 +33,23 @@ fun ErrorMessage(error: String) {
 
 fun toastMsg(context: Context, msg: String) {
     Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+}
+
+fun addSoundEffect(view: View) {
+    view.playSoundEffect(SoundEffectConstants.CLICK)
+}
+@Composable
+fun CustomRowWith2Values(modifier: Modifier = Modifier, value1: String, value2: String) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = value1,
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = value2)
+    }
 }
