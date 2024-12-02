da5a6f923f3a8cc469703ecddd8de3ae956d32e8
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-12-02 22:28:07 +0530

    Refactor code

--- app/src/main/java/com/example/jokesapp/repository/JokesRepoImpl.kt
+++ app/src/main/java/com/example/jokesapp/repository/JokesRepoImpl.kt
@@ -5,6 +5,7 @@ import com.example.jokesapp.data.local.JokesEntity
 import com.example.jokesapp.data.remote.ApiService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class JokesRepoImpl @Inject constructor(
@@ -20,35 +21,34 @@ class JokesRepoImpl @Inject constructor(
         genre: String,
         amount: Int
     ): Flow<List<JokesEntity>> {
+        val cachedJokesFlow = jokesDao.fetchUnbookmarkedJokes()
 
-        val cachedJokes = jokesDao.fetchUnbookmarkedJokes().firstOrNull()
-        return if (cachedJokes.isNullOrEmpty()) {
+        return cachedJokesFlow.onStart {
+            val cachedJokes = cachedJokesFlow.firstOrNull()
+            if (cachedJokes.isNullOrEmpty()) {
+                try {
+                    val response = apiService.fetchAllJokes(genre = genre, amount = amount)
+                    val jokesResponse = response.body()
 
-            try {
-
-                val response = apiService.fetchAllJokes(genre = genre, amount = amount)
-                val jokesResponse = response.body()
-
-                if (response.isSuccessful && jokesResponse != null) {
-                    val jokesEntityList = jokesResponse.jokes.map { joke ->
-                        JokesEntity(
-                            id = joke.id,
-                            type = joke.type,
-                            setup = joke.setup,
-                            punchLine = joke.delivery,
-                            jokeMessage = joke.joke
-                        )
+                    if (response.isSuccessful && jokesResponse != null) {
+                        val jokesEntityList = jokesResponse.jokes.map { joke ->
+                            JokesEntity(
+                                id = joke.id,
+                                type = joke.type,
+                                setup = joke.setup,
+                                punchLine = joke.delivery,
+                                jokeMessage = joke.joke
+                            )
+                        }
+                        jokesDao.insertJokesList(jokesEntityList)
                     }
-                    jokesDao.insertJokesList(jokesEntityList)
+                } catch (e: Exception) {
+                    throw Exception("Failed to fetch jokes from API, You can now only view Bookmarked Jokes")
                 }
-            } catch (e: Exception) {
-                throw Exception("Failed to fetch jokes from API, You can now only view Bookmarked Jokes")
             }
-            return jokesDao.fetchUnbookmarkedJokes()
-        } else {
-            jokesDao.fetchUnbookmarkedJokes()
         }
     }
+
 
     override suspend fun updateBookmarkStatus(id: Int, bookmarked: Boolean) {
         jokesDao.updateBookmarkStatus(jokeId = id, isBookmarked = bookmarked)
